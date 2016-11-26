3823d9ab5766955cac32beb27dda6493e24d56cb
Mariam Maarouf <mariam.mrf@hotmail.com>
2016-11-26 02:36:23 +0200

    generalize config.py

--- snowdonia/config.py
+++ snowdonia/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mariam:tiger@localhost/snowdonia'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://USERNAME:PASSWORD@localhost/snowdonia'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
 SECRET_KEY = 'secret key'
